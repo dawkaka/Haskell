e88fe7a7a53770d4c6900048d7ea14d231176ae0
dawkaka <yousiph77@gmail.com>
2023-05-13 21:26:46 +0000

    sd# Please enter the commit message for your changes. Lines starting

--- datastructures.hs
+++ datastructures.hs
@@ -1,17 +1,20 @@
 module Catamorphic where
 
-import qualified Data.Map as Map
 import Data.Array
+import Data.Map qualified as Map
 
 withdraw :: String -> Int -> Map.Map String Int -> Map.Map String Int
-withdraw id amount bank = case Map.lookup id bank of 
-                                            Nothing -> bank
-                                            Just x -> Map.insert id (x - amount) bank
+withdraw id amount bank = case Map.lookup id bank of
+  Nothing -> bank
+  Just x -> Map.insert id (x - amount) bank
 
+bank :: Map.Map String Int
 bank = withdraw "jon" 2 (Map.fromList [("jon", 100), ("doe", 200)])
 
-arr = array (1,3) [(1,"32"),(2, "10"),(3,"sds")]
-arr2 = listArray (1,10)["dsas","das","dsds","sds","ds","dsd","ds","dsd","sdd","ds"]
+arr :: Array Int String
+arr = array (1, 3) [(1, "32"), (2, "10"), (3, "sds")]
+
+arr2 = listArray (1, 10) ["dsas", "das", "dsds", "sds", "ds", "dsd", "ds", "dsd", "sdd", "ds"]
 
 -- Array lookup
 -- (!) :: Ix i => Array i e -> i -> e
@@ -19,13 +22,13 @@ arr2 = listArray (1,10)["dsas","das","dsds","sds","ds","dsd","ds","dsd","sdd","d
 -- (//) :: Ix i => Array i e -> [(i, e)] -> Array i e
 
 second = arr ! 2
-updatedArr2 = arr2 // [(5,"five")]
+
+updatedArr2 = arr2 // [(5, "five")]
 
 main = do
-    print "x"
-    print bank
-    print arr
-    print arr2
-    print second
-    print updatedArr2
-    print ws
+  print "x"
+  print bank
+  print arr
+  print arr2
+  print second
+  print updatedArr2
